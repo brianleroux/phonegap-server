62fea107f1aa2abd57bf44720bfda3bd886627fa
Michael Brooks <michael@michaelbrooks.ca>
2014-04-02 09:33:22 -0700

    Add additional output to listen, error, and request events.
    Listen will output the true IP address of the system because
    localhost is useless when connecting with real devices.
    
    Error will output an error message.
    
    Request will output the status code and requested URL.

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var pgc = require('connect-phonegap')
   , colors = require('colors')
   , log = console.log
 
-log('´¯`·.¸¸.·´¯`·.¸´¯`·.¸¸.·´¯`·.¸´¯`·'.rainbow)
-log('Listening on ' + 'http://localhost:3000'.green)
-log('´¯`·.¸¸.·´¯`·.¸´¯`·.¸¸.·´¯`·.¸´¯`·'.rainbow)
-log('Hit '.grey + 'ctrl+c'.bold + ' to exit'.grey) 
-log('Or '.grey + 'ctrl+z'.bold + ' and '.grey + 'bg'.bold + ' to background and '.grey + 'fg'.bold + ' to bring the beat back'.grey)
-log("")
 pgc.listen()
+  .on('complete', function(data) {
+    log('´¯`·.¸¸.·´¯`·.¸´¯`·.¸¸.·´¯`·.¸´¯`·'.rainbow)
+    log('Listening on ' + 'http://ADDRESS:PORT'.replace('ADDRESS', data.address).replace('PORT', data.port).green)
+    log('´¯`·.¸¸.·´¯`·.¸´¯`·.¸¸.·´¯`·.¸´¯`·'.rainbow)
+    log('Hit '.grey + 'ctrl+c'.bold + ' to exit'.grey)
+    log('Or '.grey + 'ctrl+z'.bold + ' and '.grey + 'bg'.bold + ' to background and '.grey + 'fg'.bold + ' to bring the beat back'.grey)
+    log('')
+  })
+  .on('error', function(e) {
+    log('Error', e.message.red)
+    process.exit();
+  })
+  .on('request', function(req, res) {
+    log('[' + colors.green(res.statusCode) + ']', req.url)
+  });
